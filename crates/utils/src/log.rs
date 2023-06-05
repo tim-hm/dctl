81f6744f25e5489b456cd166edc2400524af7bd4
Tim Holmes-Mitra <tim@holmie.xyz>
2023-06-05 20:31:07 +0100

    feat: disable log timestamp when not dev env
    Signed-off-by: Tim Holmes-Mitra <tim@holmie.xyz>

--- crates/utils/src/log.rs
+++ crates/utils/src/log.rs
@@ -2,6 +2,8 @@ use std::env;
 
 use env_logger::{Builder, Env};
 
+use crate::env::{get_env_name, EnvName};
+
 pub fn init() {
     let level = env::var("RUST_LOG").unwrap_or(String::from("info"));
 
@@ -9,5 +11,12 @@ pub fn init() {
         .default_filter_or(level)
         .default_write_style_or("always");
 
-    Builder::from_env(env).init();
+    let mut builder = Builder::from_env(env);
+
+    let exclude_timestamps = get_env_name() != EnvName::Dev;
+    if exclude_timestamps {
+        builder.format_timestamp(None);
+    }
+
+    builder.init();
 }
