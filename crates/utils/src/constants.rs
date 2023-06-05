8a04c555f1d0b37eb450eba3458d1ab8a7d3f1b3
Tim Holmes-Mitra <tim@holmie.xyz>
2023-06-05 20:32:08 +0100

    feat: rename socket to desktopd.socket
    Signed-off-by: Tim Holmes-Mitra <tim@holmie.xyz>

--- crates/utils/src/constants.rs
+++ crates/utils/src/constants.rs
@@ -1,3 +1,3 @@
 pub const DEV_ADDRESS: &str = "127.0.0.1";
-pub const UNIX_SOCKET_NAME: &str = "desktopd.sock";
+pub const UNIX_SOCKET_NAME: &str = "desktopd.socket";
 pub const XDG_RUNTIME_DIR: &str = "XDG_RUNTIME_DIR";
